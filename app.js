9a8f0ef2ece83b689dec1c457838bfd3ae2aebfd
TheLostSon <joekarlshorst@gmail.com>
2021-07-08 14:40:58 +0200

    * fixed errors with deleting standard users route

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -13,8 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(process.env.PORT || 5000);
 
 app.use('/', indexRouter);
 
