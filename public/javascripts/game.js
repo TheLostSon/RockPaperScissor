c6af0f6b7edd3dd2ce0e13f3dd5385c94a16d596
TheLostSon <joekarlshorst@gmail.com>
2021-07-16 15:49:15 +0200

    + animation (transform, transistion) + tables with overflow + comments in js * restructured js code * design / styles - unused codelines - title from router

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -5,26 +5,30 @@ function Game() {
     //before starting
     const start_button = document.getElementById('start');
     const restart_button = document.getElementById('restart');
-    //todo: may add onchange event for instantly displaying the name
+    const lastMatch_button = document.getElementById('last_match');
     const name_field = document.getElementById('name');
-    const outputPoints = document.getElementById('points');
+    //while playing
+    const outputPlayerPoints = document.getElementById('points_player');
+    const outputComPoints = document.getElementById('points_com');
     const outputTries = document.getElementById('tries');
-    let name = '', pointsTotal = 0, tries = 0;
-    let playerSymbol, npcSymbol;
+    let name = '', playerSymbol = {id: 0, type: ''}, npcSymbol = {id: 0, type: ''};
     const computerPlayer = {
-        0: {id: 0, type: 'Schere', img: '/images/scissors.png'}, 
-        1: {id: 1, type: 'Stein', img: '/images/rock.png'}, 
-        2: {id: 2, type: 'Papier', img: '/images/paper.png'}
+        0: {id: 1, type: 'Schere', img: '/images/scissors.png'}, 
+        1: {id: 2, type: 'Stein', img: '/images/rock.png'}, 
+        2: {id: 3, type: 'Papier', img: '/images/paper.png'}
     };
     let clickedElement;
+    let outcome = {player: '', com: ''};
+    let points = {player: 0, com: 0};
+    let pointsTotalPlayer = 0, pointsTotalCom = 0, tries = 0;
 
-    //while playing
+    //npc
     const npcImage = document.getElementById('npc_image');
-    //scissors = 0
+    //playerchoice: scissors
     const scissors_button = document.getElementById('scissors');
-    //rock = 1
+    //playerchoice: rock
     const rock_button = document.getElementById('rock');
-    //paper = 2
+    //playerchoice: paper
     const paper_button = document.getElementById('paper');
     
 
@@ -33,98 +37,135 @@ function Game() {
         start()
     }
 
-    restart_button.onclick = function(){
-        tries = 0;
-        setName()
-        resetColor()
-        countPoints(-pointsTotal)
-        npcImage.innerHTML = '';
-    }
-
+    //displays the chosen name
     function setName() {
         name = name_field.value;
-        document.getElementById('display_name').innerText = name ? name : 'Deine Auswahl';
+        document.getElementById('display_name_game').innerText = name ? name : 'Deine Auswahl';
+        document.getElementById('display_name_points').innerText = name ? name : 'Ihre Punktzahl';
     }
 
-    //todo: implement button to change name
+    //starts the game, initiates buttons
+    function start() {
+        document.getElementsByClassName('game')[0].hidden = false;
+        document.getElementsByClassName('game')[1].hidden = false;
+        document.getElementsByClassName('game')[2].hidden = false;
+        start_button.hidden = true;
+        restart_button.hidden = false;
 
-    //returns object with random number and symbol name
+        scissors_button.onclick = function(){
+            resetStyle()
+            clickedElement = scissors_button.parentElement;
+            playerSymbol = {id: 1, type: 'Schere'}
+            output(compare())
+        }   
+        rock_button.onclick = function(){
+            resetStyle()
+            clickedElement = rock_button.parentElement;
+            playerSymbol = {id: 2, type: 'Stein'}
+            output(compare())
+        }
+        paper_button.onclick = function(){
+            resetStyle()
+            clickedElement = paper_button.parentElement;
+            playerSymbol = {id: 3, type: 'Papier'}
+            output(compare())
+        }
+        restart_button.onclick = function(){
+            tries = 0;
+            setName()
+            resetStyle()
+            countPoints(-pointsTotalPlayer, -pointsTotalCom)
+            npcImage.innerHTML = '';
+            lastMatch_button.hidden = true;
+        }
+        lastMatch_button.onclick = function(){
+            showLastMatch()
+        }
+    }
+
+    //displays outcome of the recent round
+    function output(npcSymbol) {
+        npcImage.innerHTML = '<div class="background ' + outcome.com + '"><img class="btn_logo no_cursor" src="' + npcSymbol.img + '" /></div>'
+        npcImage.style.transform = 'scale(1)'
+        countPoints(points.player, points.com);
+        animate();
+    }
+
+    //sets outcome and points based on symbolId comparison (using the rock-paper-scissors-rules)
+    //returns the random npc-player
+    function compare() 
+    {
+        tries++;
+        npcSymbol = randomSymbol();
+        resetStyle();
+        if (playerSymbol.id === npcSymbol.id){
+            points = {player: 1, com: 1}
+            outcome = {player: 'draw', com: 'draw'}
+            return npcSymbol;
+        };
+        if (
+            (playerSymbol.id === 2) && (npcSymbol.id === 1)
+            || (playerSymbol.id === 1) && (npcSymbol.id === 3)
+            || (playerSymbol.id === 3) && (npcSymbol.id === 2)
+        )
+        {
+            points = {player: 3, com: 0}
+            outcome = {player: 'win', com: 'lose'}
+            return npcSymbol;
+        }
+        else {
+            points = {player: 0, com: 3}
+            outcome = {player: 'lose', com: 'win'}
+            return npcSymbol;
+        }
+    }
+
+    //returns object from (computerPlayer) with random number and symbol name
     function randomSymbol()
     {
         const number = Math.floor(Math.random() * 3);
         return computerPlayer[number];
     }
 
-    //starts the game
-    function start() {
-        //todo: with more elements change to 
-        document.getElementsByClassName('game')[0].hidden = false;
-        document.getElementsByClassName('game')[1].hidden = false;
-        start_button.hidden = true;
-        restart_button.hidden = false;
-
-        scissors_button.onclick = function(){
-            resetColor()
-            clickedElement = scissors_button;
-            playerSymbol = {id: 0, type: 'Schere'}
-            output(compare())
-        }   
-        rock_button.onclick = function(){
-            resetColor()
-            clickedElement = rock_button;
-            playerSymbol = {id: 1, type: 'Stein'}
-            output(compare())
-        }
-        paper_button.onclick = function(){
-            resetColor()
-            clickedElement = paper_button;
-            playerSymbol = {id: 2, type: 'Papier'}
-            output(compare())
-        }
-    }
-
-    function compare() 
+    //used for counting and resetting points
+    function countPoints(pointsP, pointsC)
     {
-        tries++;
-        npcSymbol = randomSymbol();
-        resetColor();
-        if (playerSymbol.id === npcSymbol.id){
-            clickedElement.classList.add('draw');
-            return {points: 1, npcSymbol, outcome: 'draw'};
-        };
-        if (
-            (playerSymbol.id === 1) && (npcSymbol.id === 0)
-            || (playerSymbol.id === 0) && (npcSymbol.id === 2)
-            || (playerSymbol.id === 2) && (npcSymbol.id === 1)
-        )
-        {
-            clickedElement.classList.add('win');
-            return {points: 3, npcSymbol, outcome: 'lose'};
-        }
-        else {
-            clickedElement.classList.add('lose');
-            return {points: 0, npcSymbol, outcome: 'win'};
-        }
-    }
-
-    function output({points, npcSymbol, outcome}) {
-        npcImage.innerHTML = '<img class="btn_logo no_cursor ' + outcome + '" src="' + npcSymbol.img + '" />'
-        countPoints(points);
-    }
-
-    function countPoints(points)
-    {
-        pointsTotal += points;
-        outputPoints.innerText= pointsTotal;
+        pointsTotalPlayer += pointsP;
+        outputPlayerPoints.innerText= pointsTotalPlayer;
+        pointsTotalCom += pointsC;
+        outputComPoints.innerText= pointsTotalCom;
         outputTries.innerText = tries;
     }
-
-    function resetColor() {
+    
+    //resets all styles applied in one round of a game
+    function resetStyle() {
         if(clickedElement) 
         {
+            clickedElement.style.transform = 'none'
             clickedElement.classList.remove('win');
             clickedElement.classList.remove('draw');
             clickedElement.classList.remove('lose');
+            npcImage.style.transform = 'scale(0)'
         }
+        lastMatch_button.hidden = true;
+    }
+
+    //animates the recent round of the game
+    function animate() {
+        clickedElement.style.transform = 'scale(0.777)'
+        clickedElement.classList.add(outcome.player);
+        const resetCount = tries;
+        setTimeout(function() {
+            if(tries === resetCount) {
+                resetStyle()
+                lastMatch_button.hidden = false;
+            }
+        }, 2500)
+    }
+
+    //displays results of the last played round 
+    function showLastMatch() {
+        clickedElement.classList.add(outcome.player)
+        npcImage.style.transform = 'scale(1)'
     }
 }
